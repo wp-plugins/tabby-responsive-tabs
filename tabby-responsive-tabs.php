74a8bfbd9bb16554d9e8754fc5cae809b7dc50ea
numeeja <numeeja@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-24 18:20:57 +0000

    * Improvements to default CSS * Addition of 'open' shortcode attribute to allow tabs other than the first to be open when the page loads * First tab now is open by default when displayed as accordion * Changed links in plugin table * Get Plugin Version function * Prevent tabs overlapping if there are too many * Remove hard coded paragraph tags in tab content & improve * Added icon font support (Font Awesome needs to be loaded by your theme or another plugin) * Added functionality to allow target url parameter to control which tab is open on page load.
    git-svn-id: https://plugins.svn.wordpress.org/tabby-responsive-tabs/trunk@937974 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tabby-responsive-tabs.php
+++ tabby-responsive-tabs.php
@@ -2,12 +2,12 @@
 /*
 Plugin Name: Tabby Responsive Tabs
 Plugin URI: http://cubecolour.co.uk/tabby-responsive-tabs
-Description: Create responsive tabs inside your posts, pages or post types by adding simple shortcodes inside the post editor. The tabs behave like an accordion on smaller screens.
+Description: Create responsive tabs inside your posts, pages or post types by adding simple shortcodes inside the post editor. The tabs change to an accordion layout on smaller screens.
 Author: cubecolour
-Version: 1.0.3
+Version: 1.1.1
 Author URI: http://cubecolour.co.uk
 
-	Tabby Responsive Tabs WordPress plugin Copyright 2013 Michael Atkins
+	Tabby Responsive Tabs WordPress plugin Copyright 2013-2014 Michael Atkins
 	
 	Licenced under the GNU GPL:
 		
@@ -54,6 +54,24 @@ Author URI: http://cubecolour.co.uk
 */
 
 // ==============================================
+//  Prevent Direct Access of this file
+// ==============================================
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if this file is accessed directly
+
+// ==============================================
+//	Get Plugin Version
+// ==============================================
+
+function cc_tabby_plugin_version() {
+	if ( ! function_exists( 'get_plugins' ) )
+		require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+	$plugin_folder = get_plugins( '/' . plugin_basename( dirname( __FILE__ ) ) );
+	$plugin_file = basename( ( __FILE__ ) );
+	return $plugin_folder[$plugin_file]['Version'];
+}
+
+// ==============================================
 //	Add Links in Plugins Table
 // ==============================================
  
@@ -64,49 +82,60 @@ function cc_tabby_meta_links( $links, $file ) {
 	
 // create the links
 	if ( $file == $plugin ) {
-		return array_merge( $links, array( '<a href="http://cubecolour.co.uk/wp">Donate</a>', '<a href="http://twitter.com/cubecolour">cubecolour on Twitter</a>' ) );
+
+		$supportlink = 'https://wordpress.org/support/plugin/tabby-responsive-tabs';
+		$donatelink = 'http://cubecolour.co.uk/wp';
+		$reviewlink = 'https://wordpress.org/support/view/plugin-reviews/tabby-responsive-tabs?rate=5#postform';
+		$twitterlink = 'http://twitter.com/cubecolour';
+		$customiselink = 'http://cubecolour.co.uk/tabby-responsive-tabs-customiser';
+		$iconstyle = 'style="-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale;"';
+		
+		if ( is_plugin_active( 'tabby-responsive-tabs-customiser/tabby-customiser.php' ) ) {
+			$customiselink = admin_url( 'options-general.php?page=tabby-settings' );
+		}
+		
+		return array_merge( $links, array(
+			'<a href="' . $supportlink . '"> <span class="dashicons dashicons-lightbulb" ' . $iconstyle . 'title="Tabby Responsive Tabs Support"></span></a>',
+			'<a href="' . $twitterlink . '"><span class="dashicons dashicons-twitter" ' . $iconstyle . 'title="Cubecolour on Twitter"></span></a>',
+			'<a href="' . $reviewlink . '"><span class="dashicons dashicons-star-filled"' . $iconstyle . 'title="Give a 5 Star Review"></span></a>',
+			'<a href="' . $donatelink . '"><span class="dashicons dashicons-heart"' . $iconstyle . 'title="Donate"></span></a>',
+			'<a href="' . $customiselink . '"><span class="dashicons dashicons-admin-appearance" ' . $iconstyle . 'title="Tabby Responsive Tabs Customizer"></span></a>'
+		) );
 	}
+	
 	return $links;
 }
 
 // ==============================================
+//
 //	Register & enqueue the stylesheet
 //	If you want to use custom styles, copy the content of the tabby.css to your child theme
 //	and stop the default styles from loading by this by adding the following line to the theme's functions.php:
 //
 //	remove_action('wp_print_styles', 'cc_tabby_css', 30);
 //
-//	to do: Possibly split into two css files - one mandatory & one replaceable
-//	or one with defaults & info on what/how to override
+//	Alternatively use the tabby responsive tabs customiser plugin
+//	available from from http:cubecolour.co.uk/tabby-responsive-tabs-customiser
+//
 // ==============================================
+
+function cc_tabby_css() {
+	wp_enqueue_style('tabby.css', plugin_dir_url(__FILE__).'css/tabby.css' , false, cc_tabby_plugin_version() );	
+}
 
 add_action('wp_print_styles', 'cc_tabby_css', 30);
 
-function cc_tabby_css() {
-	wp_enqueue_style('tabby.css', plugin_dir_url(__FILE__).'css/' . 'tabby.css' , false, '1.0.2');	
-}
-
-// ==============================================
-// Register & enqueue the script
-// ==============================================
-/*
-add_action('wp_enqueue_scripts', 'cc_tabby_js');
-function cc_tabby_js() {
-	wp_register_script( 'tabby', plugins_url() . "/" . basename(dirname(__FILE__)) . '/js/tabby.js', array('jquery'), '1.0.1', true );
-	wp_enqueue_script( 'tabby' );
-}
-*/
 // ==============================================
 // Trigger the script if it has not already been triggered on the page
 // ==============================================
 
 function cc_tabbytrigger() {
 
-	static $tabbytriggered = FALSE;
-	
+	static $tabbytriggered = FALSE; // static so only sets the value the first time it is run
+
 	if ($tabbytriggered == FALSE) {
 		echo "\n" . "<script>jQuery(document).ready(function($) { RESPONSIVEUI.responsiveTabs(); })</script>" .  "\n";
-
+		
 		$tabbytriggered = TRUE;
 	}
 }
@@ -128,40 +157,71 @@ if ($GLOBALS["reset_firsttab_flag"] === TRUE) {
 	$GLOBALS["reset_firsttab_flag"] = FALSE;
 }
 
-// extract title
-	extract(shortcode_atts(array("title" => ''), $atts));
+// extract title & whether open
+	extract(shortcode_atts(array(
+		"title" => '',
+		"open" => '',
+		"icon" => '',
+	), $atts, 'tabbytab'));
 
+	$tabtarget = sanitize_title_with_dashes( $title );
+	
+	//initialise urltarget
+	$urltarget = '';
+	
+	//grab the value of the 'target' url parameter if there is one
+	if ( isset ( $_REQUEST['target'] ) ) {
+		$urltarget = sanitize_title_with_dashes( $_REQUEST['target'] );
+	}
+	
+	//	Set Tab Panel Class - add active class if the open attribute is set or the target url parameter matches the dashed version of the tab title
+	$tabcontentclass = "tabcontent";
+
+	if ( ( $open ) || ( isset( $urltarget ) && ( $urltarget == $tabtarget ) ) ) {
+		$tabcontentclass .= " responsive-tabs__panel--active";
+	}
+
+	$addtabicon = '';
+	
+	if ( $icon ) {
+		$addtabicon = '<span class="fa fa-' . $icon . '"></span>';
+	}
+	
 // test whether this is the first tab in the group
-	if ( $firsttab == TRUE) {
+	if ( $firsttab ) {
 	
 // Set flag so we know subsequent tabs are not the first in the tab group
 		$firsttab = FALSE;
-// Build output for the first tab
-		return '<div class="responsive-tabs"><h2 class="tabtitle">' . $title . '</h2><div class="tabcontent"><p>';
+	
+// Build output if we are making the first tab
+		return '<div class="responsive-tabs">' . "\n" . '<h2 class="tabtitle">' . $addtabicon . $title . '</h2>' . "\n" . '<div class="' . $tabcontentclass . '">' . "\n";
 	}
 
     else {
-// Build output for non-first tab(s)
-		return '</p></div><h2 class="tabtitle">' . $title . '</h2><div class="tabcontent"><p>';
+// Build output if we are making a non-first tab
+		return  "\n" . '</div><h2 class="tabtitle">' . $addtabicon . $title . '</h2>' . "\n" . '<div class="' . $tabcontentclass . '">' . "\n";
 	}
 }
 
 add_shortcode('tabby', 'cc_shortcode_tabby');
-
+	
 // ==============================================
-//	SHORTCODE TO GO AFTER FINAL TABBY TAB
+//	SHORTCODE TO BE USED AFTER FINAL TABBY TAB
 //	use [tabbyending]
 // ==============================================
+
 function cc_shortcode_tabbyending( $atts, $content = null ) {
 
-	wp_enqueue_script('tabby', plugins_url('js/tabby.js', __FILE__), array('jquery'), '1.0.2', true);
+	wp_enqueue_script('tabby', plugins_url('js/tabby.js', __FILE__), array('jquery'), cc_tabby_plugin_version(), true);
 
 	add_action('wp_footer', 'cc_tabbytrigger', 20);
-
+	
 	$GLOBALS["reset_firsttab_flag"] = TRUE;
+	
+	global $cc_add_tabby_css;
+	$cc_add_tabby_css = true;
 
-	return '</p></div></div>';
+	return '</div></div>';
 }
 
 add_shortcode('tabbyending', 'cc_shortcode_tabbyending');
-
